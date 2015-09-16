1214057c5ae1828f2d59eec80310f8a3cc326640
alexcleu <alexcleu@gmail.com>
2015-09-15 19:50:38 -0700

    Added comment on the class and functions :)

--- media.py
+++ media.py
@@ -1,5 +1,17 @@
+""" the module is used to create the class Movie"""
+
 class Movie():
+ """this class is to generate instance for movies and its attribute"""
     def __init__(self, title, story, poster_image, movie_trailer):
+    
+     """Instaneous object for movie
+       
+        Args: 
+         title: the movie title
+         story: story of the movie
+         poster_image: image url of the movie
+         movie_trailer: youtube url link of the trailer
+    """
         self.title = title
         self.story = story
         self.poster_image_url = poster_image
