0892b0ab5bcfc1b8c2a48b2d17eea8f2f14cd138
alexcleu <alexcleu@gmail.com>
2015-09-15 20:16:20 -0700

    feat: added storyline and ratings Added storyline and ratings in the fresh_tomatoes.py templates. Added ratings to the Movie instances.

--- media.py
+++ media.py
@@ -1,18 +1,20 @@
 """ the module is used to create the class Movie"""
 
 class Movie():
- """this class is to generate instance for movies and its attribute"""
-    def __init__(self, title, story, poster_image, movie_trailer):
-    
-     """Instaneous object for movie
+    """Instaneous class for Movies"""
+    def __init__(self, title, story, poster_image, movie_trailer,rating):
+        """Instaneous object for movie
        
         Args: 
-         title: the movie title
-         story: story of the movie
-         poster_image: image url of the movie
-         movie_trailer: youtube url link of the trailer
-    """
+          title: the movie title
+          story: story of the movie
+          poster_image: image url of the movie
+          movie_trailer: youtube url link of the trailer
+          rating: Alex's rating: out of 5 stars
+        """
         self.title = title
         self.story = story
         self.poster_image_url = poster_image
         self.trailer_youtube_url = movie_trailer
+        self.rating = rating
+    
